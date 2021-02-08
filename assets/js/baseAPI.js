e9d5cb7e72d22e119b61bf8a216f868b9e4781c2
mwf <3247567110@qq.com>
2021-02-08 16:27:38 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,7 +7,27 @@ var baseURL = 'http://ajax.frontend.itheima.net'
 
 // 拦截所有 ajax请求 ：get/post/ajax
 // 处理参数
-$.ajaxprefilter(function (params) {
+$.ajaxPrefilter(function (options) {
+    // 1.添加根路径
     // 拼接对应环境的服务器地址
-    params.url = baseURL + params.url;
+    options.url = baseURL + options.url;
+
+    // 2.身份认证
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+
+    // 3.拦截所有响应，判断身份认证信息
+    options.complete = function (res) {
+        console.log(res.responseJSON);
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            // 1.清空本地token
+            localStorage.removeItem('token');
+            // 2.页面跳转
+            location.href = '/login.html';
+        }
+    }
 })
